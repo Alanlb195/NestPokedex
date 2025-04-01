ae2b19f1d9e8d884f1f68d4a3e687504bab47906
alanlb195 <alanlopezbarrientos@live.com.mx>
2025-04-01 01:15:13 -0500

    Feat - Get pokemons - Pagination and PaginationDto

--- src/pokemon/pokemon.controller.ts
+++ src/pokemon/pokemon.controller.ts
@@ -3,6 +3,7 @@ import { PokemonService } from './pokemon.service';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { ParseMongoIdPipe } from 'src/common/pipes/parse-mongo-id.pipe';
+import { PaginationDto } from 'src/common/dtos/pagination.dto';
 
 @Controller('pokemon')
 export class PokemonController {
@@ -14,10 +15,8 @@ export class PokemonController {
   }
 
   @Get()
-  findAll( @Query() queryParameters ) {
-    console.log({queryParameters});
-    
-    return this.pokemonService.findAll();
+  findAll( @Query() paginationDto: PaginationDto ) {
+    return this.pokemonService.findAll(paginationDto);
   }
 
   @Get(':term')
